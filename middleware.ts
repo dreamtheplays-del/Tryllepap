9014d7a4ed9f01b262eb4031191f25c45f11f04a
dreamtheplays-del <dreamtheplays@gmail.com>
2026-05-10 23:13:32 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -28,5 +28,7 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)'],
+  matcher: [
+    '/((?!_next/static|_next/image|favicon.ico|auth/callback|auth/confirm|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+  ],
 }
